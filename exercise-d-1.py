e78c6fb7999edb98276bc030c58ee0f2675a8dc7
DoubleW2w <1049951363@qq.com>
2024-11-06 02:00:18 +0800

    ✨ 第一天练习内容

---
+++ exercise-d-1.py
@@ -0,0 +1,41 @@
+# 1.检查你使用的python版本
+# pytnon --version  或者 python -V
+
+"""
+2. 操作数是3和4，请写出运算
+"""
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+"""
+3. 写出字符串
+"""
+print("your name: " + "Lyh")
+print("your family name: "+ "L")
+print("your country: "+ "china")
+print("I am enjoying 30 days of python")
+
+"""
+4. 判断数据类型
+"""
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("my name"))
+
+
+"""
+5. 计算(2,3)和(10,8)的距离
+"""
+print(((10-2)**2 + (8-3)**2)**(1/2))
+
+
+
+
